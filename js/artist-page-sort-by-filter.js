9e6ae250fa7f075a267d9613e693a8f81f68c523
Raviegupta <raviegupta153@gmail.com>
2023-09-28 13:55:39 +0530

    BUILD: ARTIST PAGE => FILTER SECTION

---
+++ js/artist-page-sort-by-filter.js
@@ -0,0 +1,13 @@
+const sortButton = document.querySelector('.sort-by-wrapper');
+const arrowUp = document.querySelector('.sort-by-filter-arrow-up');
+const arrowDown = document.querySelector('.sort-by-filter-arrow-down');
+const sortingOptions = document.querySelector('.sorting-options-box');
+
+
+sortButton.addEventListener('click', () => {
+    console.log('hello ravie');
+
+    arrowDown.classList.toggle('hidden');
+    arrowUp.classList.toggle('hidden');
+    sortingOptions.classList.toggle('hidden');
+})
