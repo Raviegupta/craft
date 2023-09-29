4fd6a44961f8fae1db80974f9ad36ceed1be23ce
Raviegupta <raviegupta153@gmail.com>
2023-09-29 15:41:05 +0530

    BUILD: ARTIST PAGE => FILTER SECTION FOR LOW PIXEL DEVICES

--- js/artist-page-sort-by-filter.js
+++ js/artist-page-sort-by-filter.js
@@ -3,10 +3,27 @@ const arrowUp = document.querySelector('.sort-by-filter-arrow-up');
 const arrowDown = document.querySelector('.sort-by-filter-arrow-down');
 const sortingOptions = document.querySelector('.sorting-options-box');
 
+const sortButtonMini = document.querySelector('.sort-by-wrapper-mini-devices');
+const arrowUpMini = document.querySelector('.sort-by-filter-arrow-up-mini-devices');
+const arrowDownMini = document.querySelector('.sort-by-filter-arrow-down-mini-devices');
+const sortingOptionsMini = document.querySelector('.sorting-options-box-mini-devices');
+
+
+
 sortButton.addEventListener('click', () => {
     console.log('hello ravie');
 
     arrowDown.classList.toggle('hidden');
     arrowUp.classList.toggle('hidden');
     sortingOptions.classList.toggle('hidden');
+})
+
+
+
+sortButtonMini.addEventListener('click', () => {
+    console.log('hello ravie');
+
+    arrowDownMini.classList.toggle('hidden');
+    arrowUpMini.classList.toggle('hidden');
+    sortingOptionsMini.classList.toggle('hidden');
 })
