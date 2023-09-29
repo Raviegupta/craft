5c89baf08f4e1e7af45833f2ceed34a1844a8f99
Raviegupta <raviegupta153@gmail.com>
2023-09-29 12:32:10 +0530

    BUILD: ARTIST PAGE => FILTER SECTION FOR LOW PIXEL DEVICES

---
+++ js/artist-page-filter-lower-devices.js
@@ -0,0 +1,9 @@
+// FILTER SECTIONB FOR LOWER DEVICES
+const filterMiniDevices = document.querySelector('.artist-page-filter-section-lower-devices');
+
+console.log(filterMiniDevices);
+
+
+filterMiniDevices.addEventListener('click', () => {
+    console.log('ravie .........................')
+})
