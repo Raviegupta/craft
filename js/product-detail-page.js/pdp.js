6f2b31743b5cfc5fa416916a117924edd45ccba4
Raviegupta <raviegupta153@gmail.com>
2023-10-02 16:00:52 +0530

    FEAT: PDP => About the creation => show more and show less Feauture

--- js/product-detail-page.js/pdp.js
+++ js/product-detail-page.js/pdp.js
@@ -3,6 +3,7 @@ const asideOneUnitAns = document.querySelectorAll('.pdp-aside-last-unit-ans');
 
 const aboutCreatorPara = document.querySelector('.about-creation-para');
 const aboutCreatorShowMoreBtn = document.querySelector('.about-creation-show-more');
+const aboutCreatorShowLessBtn = document.querySelector('.about-creation-show-less');
 
 
 // Beutiful ❤️❤️❤️
@@ -17,8 +18,8 @@ for(let i = 0; i<asideOneUnitQ.length; i++) {
 const aboutCreatorParaText = aboutCreatorPara.textContent;
 console.log(aboutCreatorParaText.length);
 
-if(aboutCreatorParaText.length > 300 ) {
-    aboutCreatorPara.textContent = aboutCreatorParaText.slice(0, 300) + '...';
+if(aboutCreatorParaText.length > 450 ) {
+    aboutCreatorPara.textContent = aboutCreatorParaText.slice(0, 450) + '...';
     console.log('hello ')
 } else {
     aboutCreatorPara.textContent = aboutCreatorParaText;
@@ -26,7 +27,13 @@ if(aboutCreatorParaText.length > 300 ) {
 }
 
 aboutCreatorShowMoreBtn.addEventListener('click', () => {
-    console.log('raviiiiiiiiiiiiiiiiiiiii');
-
     aboutCreatorPara.textContent = aboutCreatorParaText;
+    aboutCreatorShowMoreBtn.classList.add('hidden');
+    aboutCreatorShowLessBtn.classList.remove('hidden');
+})
+
+aboutCreatorShowLessBtn.addEventListener('click', () => {
+    aboutCreatorPara.textContent = aboutCreatorParaText.slice(0, 450) + '...';
+    aboutCreatorShowLessBtn.classList.add('hidden');
+    aboutCreatorShowMoreBtn.classList.remove('hidden');
 })
