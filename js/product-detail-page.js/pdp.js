2fde397ae24ae13187977dedd8b5425c9672a4cc
Raviegupta <raviegupta153@gmail.com>
2023-10-02 15:47:16 +0530

    STYLE: PDP => About the creation => show more

--- js/product-detail-page.js/pdp.js
+++ js/product-detail-page.js/pdp.js
@@ -1,6 +1,9 @@
 const asideOneUnitQ = document.querySelectorAll('.pdp-aside-last-unit-ques');
 const asideOneUnitAns = document.querySelectorAll('.pdp-aside-last-unit-ans');
 
+const aboutCreatorPara = document.querySelector('.about-creation-para');
+const aboutCreatorShowMoreBtn = document.querySelector('.about-creation-show-more');
+
 
 // Beutiful ❤️❤️❤️
 for(let i = 0; i<asideOneUnitQ.length; i++) {
@@ -8,3 +11,22 @@ for(let i = 0; i<asideOneUnitQ.length; i++) {
         asideOneUnitAns[i].classList.toggle('hidden');
     })
 }
+
+
+
+const aboutCreatorParaText = aboutCreatorPara.textContent;
+console.log(aboutCreatorParaText.length);
+
+if(aboutCreatorParaText.length > 300 ) {
+    aboutCreatorPara.textContent = aboutCreatorParaText.slice(0, 300) + '...';
+    console.log('hello ')
+} else {
+    aboutCreatorPara.textContent = aboutCreatorParaText;
+
+}
+
+aboutCreatorShowMoreBtn.addEventListener('click', () => {
+    console.log('raviiiiiiiiiiiiiiiiiiiii');
+
+    aboutCreatorPara.textContent = aboutCreatorParaText;
+})
